edad11a39b5153020b4623a8cc013c91b7bdd827
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-10-27 19:15:51 +0300

    add appliance creator factory

--- src/main/java/by/tc/task01/dao/creator/ApplianceCreatorFactory.java
+++ src/main/java/by/tc/task01/dao/creator/ApplianceCreatorFactory.java
@@ -1,6 +1,6 @@
 package by.tc.task01.dao.creator;
 
-import by.tc.task01.dao.creator.creators.OvenCreator;
+import by.tc.task01.dao.creator.creators.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +11,20 @@ public class ApplianceCreatorFactory {
 
     private Map<String, ApplianceCreator> creators = new HashMap<>();
 
-    private ApplianceCreatorFactory{
+    private ApplianceCreatorFactory() {
+        creators.put("laptop", new LaptorCreator());
         creators.put("oven", new OvenCreator());
+        creators.put("refrigerator", new RefrigeratorCreator());
+        creators.put("speakers", new SpeakersCreator());
+        creators.put("tabletPC", new TabletPCCreator());
+        creators.put("vacuumCleaner", new VacuumCleanerCreator());
+    }
+
+    public static ApplianceCreatorFactory getInstance(){
+        return instance;
+    }
+
+    public ApplianceCreator getCreator(String applianceName){
+        return creators.get(applianceName);
     }
 }
--- src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
+++ src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
@@ -3,17 +3,54 @@ package by.tc.task01.dao.impl;
 import by.tc.task01.dao.ApplianceDAO;
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 
 public class ApplianceDAOImpl implements ApplianceDAO{
 
+	private static final String APPLIANCES_DB = "./src/main/resources/appliances_db.xml";
+
 	@Override
 	public Appliance find(Criteria criteria) {
+		try{
+
+			// Create document builder
+			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+			// Create DOM tree document from a file
+			Document document = documentBuilder.parse(new File(APPLIANCES_DB));
+
+			// Get root element
+			Node root = document.getDocumentElement();
+
+			// Get all sub elements
+			NodeList nodes = root.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++){
+				Node node = nodes.item(i);
+				if (node.getNodeType() == Node.ELEMENT_NODE){
+					if (node.getNodeName().equalsIgnoreCase(criteria.getGroupSearchName())){
 
 
 
+						//todo
+						//appliance creator factory
+						//matches checker criterias
+					}
 
 
-		// you may add your own code here
+				}
+			}
+		}
+		catch (Exception e){
+			return null;
+		}
 		return null;
 	}
 	
